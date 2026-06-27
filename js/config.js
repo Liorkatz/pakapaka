6f901cee37bf2a3da2c3f044b174a6762e477b66
Lior Katz <Liorkatz@gmail.com>
2026-06-27 23:11:07 +0300

    Bump version to 1.17 and add save lock

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
-const VERSION = '1.16';
+const VERSION = '1.17';
 const STORAGE_KEY = 'pekaot_barcode_v3';
 const SHARED_FAV_KEY = 'pakapaka_shared_favorites_v1';
 const DEPARTMENT_KEY = 'pakapaka_department_v1';
@@ -11,3 +11,4 @@ let saveTarget = activeTab;
 let sharedItems = [];
 let sharedLoaded = false;
 let touchState = null;
+let isSaving = false;
